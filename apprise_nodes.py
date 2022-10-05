362e232e3fea21e15311aa49569519b4913e57a8
PartTimeDataScientist <git@parttimedatascientist.de>
2022-10-06 01:20:28 +0200

    Improved error handling and feedback

--- apprise_nodes.py
+++ apprise_nodes.py
@@ -42,13 +42,24 @@ class AppriseNotifierSingle:
 
         apobj = apprise.Apprise()
 
-        LOGGER.info("Adding url" + self.apprise_url + "to Apprise...")
-        apobj.add(self.apprise_url)
+        LOGGER.info("Adding url \"" + self.apprise_url + "\" to Apprise...")
 
-        apobj.notify(
+        config_result = apobj.add(self.apprise_url)
+
+        if not config_result:
+            raise ValueError("URL \"" + self.apprise_url + "\" could not be added to Apprise!")
+
+        push_result = apobj.notify(
             title=self.apprise_title,
             body=self.apprise_body
         )
+
+        match push_result:
+            case None:
+                raise RuntimeError("Notification could not be sent!")
+            case False:
+                LOGGER.error("Notification could not be sent!")
+                exec_context.set_warning("Notification could not be sent!")
 
         apobj.clear()
 
@@ -59,7 +70,7 @@ class AppriseNotifierMultiple:
     """
     This is another rather simple Node in the collection of the *Apprise Nodes*.
     It can be used to send a simple notification to *up to five* service URLs.  The connection to a workflow occurs through the (hidden) variable ports.
-    Unused services should stay at "empty:", otherwise it is tried to add the content to Apprise!
+    Unused services should stay at "empty:", otherwise it is tried to add the content to Apprise and may raise errors!
 
     See Apprise documentation for more details regarding supported notification URLs: https://github.com/caronc/apprise#supported-notifications
 
@@ -104,15 +115,31 @@ class AppriseNotifierMultiple:
 
         urls = [self.apprise_url1, self.apprise_url2, self.apprise_url3, self.apprise_url4, self.apprise_url5]
         LOGGER.info(urls)
+        all_ok = True
         for url in urls:
             if not (url.startswith("empty:")):
-                LOGGER.info("Adding url " + url + " to Apprise...")
-                apobj.add(url)
+                LOGGER.info("Adding url \"" + url + "\" to Apprise...")
 
-        apobj.notify(
+                config_result = apobj.add(url)
+
+                if not config_result:
+                    LOGGER.warn("URL \"" + url + "\" could not be added to Apprise!")
+                    all_ok = False
+
+        if not(all_ok):
+            exec_context.set_warning("At least one URL could not bee added to Apprise")
+
+        push_result = apobj.notify(
             title=self.apprise_title,
             body=self.apprise_body
         )
+
+        match push_result:
+            case None:
+                raise RuntimeError("Notifications could not be sent at all!")
+            case False:
+                LOGGER.error("At least one notification could not be sent!")
+                exec_context.set_warning("At least one notification could not be sent!")
 
         apobj.clear()
 
@@ -123,7 +150,7 @@ class AppriseNotifierMultiple:
 class AppriseNotifierTable1:
     """
     This node can be used to send out a series of notifications defined in a table. It expects a table with at least three columns, one containing the notification URL and the other two with the message titles and message bodies.
-    This node can e.g. be used to notify the whole team about an issue with everyone receiving the notification on his or her preferred service.
+    A typical application could be to notify the whole team about an issue with everyone receiving the notification on his or her preferred service.
 
     See Apprise documentation for more details regarding supported notification URLs: https://github.com/caronc/apprise#supported-notifications
 
@@ -152,7 +179,7 @@ class AppriseNotifierTable1:
         include_none_column=False # whether to enable None as a selectable option, which returns "<none>"
     )
 
-    def configure(self, configure_context, input_schema_1):
+    def configure(self, config_context, input_schema_1):
         pass 
 
     def execute(self, exec_context, input_1):
@@ -165,17 +192,37 @@ class AppriseNotifierTable1:
 
         apobj = apprise.Apprise()
 
+        all_config_ok = True
+        all_push_ok = True
         for index, row in input_1_pandas.iterrows():
             if not (row[configs_name].startswith("empty:")):
                 LOGGER.info("Sending notification to " + row[configs_name])
-                apobj.add(row[configs_name])
 
-                apobj.notify(
+                config_result = apobj.add(row[configs_name])
+
+                if not config_result:
+                    LOGGER.warn("Error in row " + index + " - URL \"" + row[configs_name] + "\" could not be added to Apprise!")
+                    all_config_ok = False
+
+                push_result = apobj.notify(
                     title=row[titles_name],
                     body=row[bodies_name]
                 )
 
+                match push_result:
+                    case None:
+                        all_push_ok = False
+                        LOGGER.warn("Error in row " + index + " Notification could not be sent!")
+                        exec_context.set_warning("Error in row " + index + " Notification could not be sent!")
+                    case False:
+                        all_push_ok = False
+                        LOGGER.error("Error in row " + index + " Notification could not be sent!")
+                        exec_context.set_warning("Error in row " + index + " Notification could not be sent!")
+
                 apobj.clear()
+
+        if not(all_config_ok & all_push_ok):
+            exec_context.set_warning("At least one row produced an error - see log for details!")
 
         return
 
@@ -206,14 +253,30 @@ class AppriseNotifierVariable:
         except:
             raise ValueError("FlowVariable 'apprise_config' could not be found")
 
+        all_config_ok = True
         for line in config.split("\n"):
-            LOGGER.info("Adding url " + line + " to Apprise...")
-            apobj.add(line)
+            LOGGER.info("Adding url \"" + line + "\" to Apprise...")
+            config_result = apobj.add(line)
 
-        apobj.notify(
+            if not config_result:
+                LOGGER.warn("URL \"" + line + "\" could not be added to Apprise!")
+                all_config_ok = False
+
+        all_push_ok = True
+        push_result = apobj.notify(
             title=self.apprise_title,
             body=self.apprise_body
         )
+
+        match push_result:
+            case None:
+                raise RuntimeError("Notifications could not be sent at all!")
+            case False:
+                LOGGER.error("At least one notification could not be sent!")
+                exec_context.set_warning("At least one notification could not be sent!")
+
+        if not(all_config_ok & all_push_ok):
+            exec_context.set_warning("At least one service produced an error - see log for more details!")
 
         apobj.clear()
 
